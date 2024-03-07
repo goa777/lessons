680efa090fbc0970233d2cb1f4d8854fcf3a5e10
ievaBIT <ieva.tendziagolskytes@gmail.com>
2024-03-07 15:34:36 +0200

    console

--- 008/app.js
+++ 008/app.js
@@ -1,5 +1,7 @@
 console.log('Hello from far away!');
 
+let g;
+
 let a = 10;
 let b = '10';
 let c = 'hello';
@@ -11,6 +13,28 @@ let y = 'tis';
 let l1 = 'B';
 let l2 = 'a'
 
-console.log(b - c);
+// console.log(b - c);
 
 
+// console.log((0.3 + 0.6).toFixed (5));
+
+console.log(0.3 + 0.6);
+
+a= 50;
+g= a;
+
+console.log(a, g);
+console.log(g + a);
+console.log(g + b);
+
+let t = true;
+let f = false;
+
+console.log(t + f);
+console.log(t * f);
+console.log(t / f);
+
+console.log(!!a);
+console.log(!!b);
+
+console.log(t && f);
