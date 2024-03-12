36f8498ac72f595d51bdd74ae4d073df29cb1a61
ievaBIT <ieva.tendziagolskytes@gmail.com>
2024-03-12 16:08:36 +0200

    let

--- 009/app.js
+++ 009/app.js
@@ -1,4 +1,4 @@
-console.log ('hello from app.js!');
+console.log ('Hello from app.js!');
 
 
 
@@ -48,3 +48,84 @@ console.log("'hello' || 'world':", 'hello' || 'world');
 console.log("'hello' && 'world':", 'hello' && 'world');
 console.log("'' || 'world':", '' || 'world');
 console.log("'' && 'world':", '' && 'world');
+
+
+
+
+console.clear();
+
+
+
+
+let a = 0;
+a || console.log('a is 0');
+a && console.log('a is not 0');
+
+let b = 1 + 2;
+let c = 1 + 2 * 3;
+let d = (1 + 2) * 3;
+
+console.log('b:', b);
+console.log('c:', c);
+console.log('d:', d);
+
+let e = 9 % 7;
+console.log('e:', e);
+
+// let f = 2 ** 3;
+// console.log('f:', f);
+
+let g = 7;
+let h = 3;
+
+g++;
+h--;
+
+console.log('g:', g);
+console.log('h:', h);
+
+
+let i = 27;
+console.log('i:', ++i);
+console.log('i:', i);
+
+let j = 2;
+
+let result = ++j * ++j; // 3 * 4
+result = j++ * ++j; // 4 * 6
+
+console.log('result:', result);
+
+let x = 1;
+
+--x || console.log('LOGGED');
+
+console.clear();
+
+console.log('1 < 2:', 1 < 2);
+console.log('1 > 2:', 1 > 2);
+console.log('1 <= 2:', 1 <= 2);
+console.log('1 >= 2:', 1 >= 2);
+
+console.log('1 < 1:', 1 < 1);
+console.log('1 > 1:', 1 > 1);
+console.log('1 <= 1:', 1 <= 1);
+console.log('1 >= 1:', 1 >= 1);
+
+console.log('1 == 1:', 1 == 1);
+console.log('1 == 2:', 1 == 2);
+console.log('1 != 2:', 1 != 2);
+console.log('1 != 1:', 1 != 1);
+
+
+console.log("'1' == 1:", '1' == 1);
+console.log("'1' === 1:", '1' === 1);
+console.log('true == 1', true == 1); // true is 1
+console.log('true === 1', true === 1);
+console.log('NaN == NaN:', NaN == NaN);
+let m = 1;
+m++;
+console.log('m:', m);
+console.log('is NaN', isNaN(m));
+console.log('true !== 1', true !== 1);
+console.log('true != 1', true != 1);
