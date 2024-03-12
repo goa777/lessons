4b3ea023dccfe8a36b3cc9f4c3549aa0900e0816
ievaBIT <ieva.tendziagolskytes@gmail.com>
2024-03-12 16:05:50 +0200

    JS- // OR ||  // AND &&  // NOT !  // NOT with numbers and strings  // OR AND with numbers  // OR AND with strings

---
+++ 009/app.js
@@ -0,0 +1,50 @@
+console.log ('hello from app.js!');
+
+
+
+// OR ||
+console.log('true || true:', true || true);
+console.log('true || false:', true || false);
+console.log('false || true:', false || true);
+console.log('false || false:', false || false);
+
+
+// AND &&
+console.log('true && true:', true && true);
+console.log('true && false:', true && false);
+console.log('false && true:', false && true);
+console.log('false && false:', false && false);
+
+
+// NOT !
+console.log('!true:', !true);
+console.log('!false:', !false);
+console.log('!!true:', !!true);
+console.log('!!false:', !!false);
+
+
+// NOT with numbers and strings
+console.log('!0:', !0);
+console.log('!1:', !1);
+console.log('!187:', !187);
+console.log('!!-187:', !!-187);
+console.log("!'':", !'');
+console.log("!'hello':", !'hello');
+console.log("!!' ':", !!' ');
+
+
+// undefined type
+console.log('!undefined:', !undefined);
+
+
+// OR AND with numbers
+console.log('25 || 36:', 25 || 36);
+console.log('25 && 36:', 25 && 36);
+console.log('0 || 36:', 0 || 36);
+console.log('0 && 36:', 0 && 36);
+
+// OR AND with strings
+console.log("'hello' || 'world':", 'hello' || 'world');
+console.log("'hello' && 'world':", 'hello' && 'world');
+console.log("'' || 'world':", '' || 'world');
+console.log("'' && 'world':", '' && 'world');
