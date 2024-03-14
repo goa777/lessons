5612fbb1f68ab79b2543f41e85962047866527b9
ievaBIT <ieva.tendziagolskytes@gmail.com>
2024-03-14 13:33:13 +0200

    uzd.

--- 011/uz.js
+++ 011/uz.js
@@ -1,5 +1,6 @@
-console.log('hello, how are you?');
+console.log('Laba, diena!');
 
+console.log('------------3----------------');
 
 function rand(min, max) {
     min = Math.ceil(min);
@@ -7,70 +8,13 @@ function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-/*1.*/
-
-let one = rand(0, 4);
-let two = rand(0, 4);
-
-console.log(one, two);
-
-let result;
-
-
-if (one > two) {
-    let result = one / two;
-} else {
-    let result = two / one;
-}
-
-
-console.log (result);
-
-console.clear();
-
-
-
-
-
-/*2.*/
-
-let A = rand(0, 25);
-let B = rand(0, 25);
-let C = rand(0, 25);
-
-console.log(A, B, C);
-console.log (result);
-
-if (A < B && A > C || A > B && A < C);
-else if (B  < A && B > C || B > A && B < C);
-else if (A === B || A === C || B === C);
-else if (A === B && A === C && B === C);
-else echo ();
-
-console.clear();
-
-
-/*3 uzd.*/
 
 let a = rand(1, 10);
 let b = rand(1, 10);
 let c = rand(1, 10);
 
-console.log(a, b, c);
-console.log (result);
+/*3uzd.*/
 
-if (a + b > c && a + c > b && b + c > a);
-else echo ();
-
-console.clear();
-
-
-/*
-
-
-let a = rand(1, 10);
-let b = rand(1, 10);
-let c = rand(1, 10);
 
 console.log(a, b, c);
 
@@ -80,9 +24,6 @@ console.log(arGaunasi ? 'Gaunasi' : 'Negaunasi');
 
 let arGaunasi2 = (a + b + c) / Math.max(a, b, c) < 2;
 console.log(arGaunasi ? 'Gaunasi' : 'Negaunasi');
-
-*/
-
 
 
 
@@ -119,4 +60,3 @@ nuliai = 4 - vienetai - dvejetai;
 console.log('nuliu:', nuliai);
 console.log('vienetu:', vienetai);
 console.log('dvejetu:', dvejetai);
-
