e38680967e5f545b5ee4c36fa5fdd9b7820c9e8b
ievaBIT <ieva.tendziagolskytes@gmail.com>
2024-03-21 13:57:51 +0200

    5uzt

--- 015/app.js
+++ 015/app.js
@@ -1,21 +1,8 @@
 function rand(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-//  1.
-
-let a = rand(1, 6);
-let b = rand(1, 6);
-console.log (a + b);
-
-
-let sum = a + b;
-
-
-
-
 
 
 /*5. (BOSO lygis) Sugeneruokite tris atsitiktinius skaičius nuo 1 iki 7. 
